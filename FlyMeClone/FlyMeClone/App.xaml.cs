b6c93bdc686e5a0c7b4f83945d269f1a72a96cb8
Ivan Janečka <ivanj9@gmail.com>
2020-04-26 12:47:21 +0200

    Add project files.

---
+++ FlyMeClone/FlyMeClone/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FlyMeClone.Services;
+using FlyMeClone.Views;
+
+namespace FlyMeClone
+{
+    public partial class App : Application
+    {
+        public static string REMEMBER_ME = "remember_me";
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FlyMeClone/FlyMeClone/AppShell.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FlyMeClone
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private async void MenuItem_Clicked(object sender, System.EventArgs e)
+        {
+            await SecureStorage.SetAsync(App.REMEMBER_ME, false.ToString());
+            await Shell.Current.GoToAsync("///login");
+            Shell.SetFlyoutBehavior(Shell.Current, FlyoutBehavior.Disabled);
+            Shell.Current.FlyoutIsPresented = false;
+        }
+
+    }
+}
